ca0779980edec3e6cd7d7c7b2b6e8701c4f183b9
Tvman <elijahpauley702@gmail.com>
2026-09-09 18:38:36 -0400

    Add files via upload

--- package/Runtime/Core/TvAntiViolation.cs
+++ package/Runtime/Core/TvAntiViolation.cs
@@ -1,5 +1,5 @@
 namespace TvAnti {
- public enum TvAntiViolation { None, ImpossibleSpeed, ImpossibleAcceleration, Teleport, VerticalAnomaly, UnexpectedAssembly, ClockAnomaly }
+ public enum TvAntiViolation { None, ImpossibleSpeed, ImpossibleAcceleration, Teleport, VerticalAnomaly, UnexpectedAssembly, UnexpectedNativeModule, ClockAnomaly }
  public readonly struct TvAntiEvent {
   public readonly TvAntiViolation Type; public readonly float Severity; public readonly string Detail;
   public TvAntiEvent(TvAntiViolation type,float severity,string detail){Type=type;Severity=severity;Detail=detail;}
